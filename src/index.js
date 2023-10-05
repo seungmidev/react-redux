a59a0ecda63bb6fc91fb8b9b814fb09260bc1058
seungmidev <seungmilee.dev@gmail.com>
2023-10-05 12:30:22 +0100

    create vanilla redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore } from "redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const ADD = "ADD";
+const MINUS = "MINUS";
+
+number.innerText = 0;
+
+const countModifier = (count = 0, action) => { // Ruducer: modify data, return data(state) of application
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  }
+}; 
+const countStore = createStore(countModifier); // Store data, It is required reducer
+
+const onChange = () => {
+  number.innerText = countStore.getState();
+}
+countStore.subscribe(onChange); // Able to know the changes in the store
+
+// Send a message to reducer to communicate by using action
+add.addEventListener("click", () => countStore.dispatch({type: ADD})); // actions must be plain objects, actions must have a type
+minus.addEventListener("click", () => countStore.dispatch({type: MINUS}));
